414f41449a973856a991b02c5357f662e56b3c9b
Isaac Layton <isaacdlayton@gmail.com>
2018-03-27 23:22:32 -0700

    going to try smaller random forest subsets

--- ForestScript.R
+++ ForestScript.R
@@ -6,6 +6,8 @@ library(data.table)
 
 #This script will return the random forest implementation
 #It's too time-consuming to run on my computer, so I'll run it on an EC2 instance and save the results as an .RData file
+nfolds = 30
+subsets = kFolds(df[,1], nfolds)
 rf.digits = list()
 rf.predicts = list()
 rf_df = data.table(df)
