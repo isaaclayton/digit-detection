d431342b9dbb49663348932721fd6927ddcba963
Isaac Layton <isaacdlayton@gmail.com>
2018-03-27 23:41:15 -0700

    debugging random forest script

--- ForestScript.R
+++ ForestScript.R
@@ -1,6 +1,9 @@
 load(".RData")
 if(!require(randomForest)) install.packages('randomForest', repos = "http://cran.us.r-project.org")
+if(!require(doMC)) install.packages('doMC', repos = "http://cran.us.r-project.org")
 library(randomForest)
+library(doMC)
+registerDoMC()
 
 #This script will return the random forest implementation
 #It's too time-consuming to run on my computer, so I'll run it on an EC2 instance and save the results as an .RData file
@@ -11,7 +14,11 @@ rf.predicts = list()
 df$V1 = as.factor(df$V1)
 for(i in 1:nfolds) {
   print(paste("Starting Fold", i))
-  rf.digits[[i]] = randomForest(df[subsets!=i, 2:151],df[subsets!=i,1], mtry=20, importance=T, keep.forest = TRUE, type='classification')
+  rf <- foreach(ntree=rep(25000, 6), .combine=combine, .packages='randomForest') %dopar%
+    randomForest(x, y, ntree=ntree)
+  rf.digits[[i]] = foreach(ntree=501, .combine=combine, .multicombine=TRUE, .packages='randomForest') %dopar% {
+    randomForest(df[subsets!=i, 2:151],df[subsets!=i,1], mtry=20, importance=T, keep.forest = TRUE, ntree=ntree)
+  }
   rf.predicts[[i]] = predict(rf.digits[[i]], df[subsets==i,2:151])
   print(paste("Done with Fold", i))
 }
