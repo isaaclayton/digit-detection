dfe7ba218cc5b0ef1efe367441da011524c81bc3
Isaac Layton <isaacdlayton@gmail.com>
2018-03-27 23:31:19 -0700

    debugging random forest script

--- ForestScript.R
+++ ForestScript.R
@@ -11,7 +11,7 @@ rf.predicts = list()
 df$V1 = as.factor(df$V1)
 for(i in 1:nfolds) {
   print(paste("Starting Fold", i))
-  rf.digits[[i]] = randomForest(df[subsets!=i, 2:151],df[subsets!=i,1], mtry=20, importance=T, keep.forest = TRUE)
+  rf.digits[[i]] = randomForest(df[subsets!=i, 2:151],df[subsets!=i,1], mtry=20, importance=T, keep.forest = TRUE, type='classification')
   rf.predicts[[i]] = predict(rf.digits[[i]], df[subsets==i,2:151])
   print(paste("Done with Fold", i))
 }
