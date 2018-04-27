bab4f12647a150fbb631fb3ea0af29a690987ce9
Isaac Layton <isaacdlayton@gmail.com>
2018-04-26 23:26:02 -0700

    can't even function

--- knnScript.R
+++ knnScript.R
@@ -3,20 +3,16 @@ if(!require(flexclust)) install.packages('flexclust', repos = "http://cran.us.r-
 library(flexclust)
 
 knnclass = function(x, trainx, trainy, k) {
-  y_set = levels(as.factor(trainy))
+  y = as.factor(trainy)
+  y_set = levels(y)
   dists = apply(dist2(x,trainx), 1, order)
-  #new = apply(dist2(x,trainx), 1, function(g) 
-  #y_set[which.max(tabulate(as.factor(trainy[match(sort(g, partial=1:k)[1:k],g)])))])
-  #new = apply(dists, 1, function(g) 
-  # y_set[which.max(table(trainy[order(g)[1:k]]))])
   predictions = list()
   for (i in k) {
-    predictions[[i]] = apply(dists, 1, function(g)
-      y_set[which.max(tabulate(trainy[dists[1:i]]+1))])
+    predictions[[i]] = apply(dists, 2, function(g)
+      y_set[which.max(tabulate(match(y[g[1:i]], y_set)))])
   }
   return(predictions)
 }
-
 knntests = knnclass(validation[,2:226],training[,2:226], training[,1], k=1:15)
 print("Done")
 save.image("knn.RData")
