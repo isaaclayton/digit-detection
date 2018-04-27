ce381c8f807109896a0746396dd5c733318122b6
Isaac Layton <isaacdlayton@gmail.com>
2018-04-26 21:36:00 -0700

    conjuction junction what's your KNN function

--- knnScript.R
+++ knnScript.R
@@ -2,12 +2,22 @@ load(".RData")
 if(!require(flexclust)) install.packages('flexclust', repos = "http://cran.us.r-project.org")
 library(flexclust)
 
-knntests = list() 
-for(i in 1:15){
-  knntests[[i]] = knnclass(validation[,2:226],
-                           training[,2:226], training[,1],
-                           k=i)
-  print("One more done!")
+knnclass = function(x, trainx, trainy, k) {
+  y_set = levels(as.factor(trainy))
+  dists = apply(dist2(x,trainx), 1, order)
+  #new = apply(dist2(x,trainx), 1, function(g) 
+  #y_set[which.max(tabulate(as.factor(trainy[match(sort(g, partial=1:k)[1:k],g)])))])
+  #new = apply(dists, 1, function(g) 
+  # y_set[which.max(table(trainy[order(g)[1:k]]))])
+  predictions = list()
+  for (i in k) {
+    predictions[[i]] = new = apply(dists, 1, function(g)
+      y_set[which.max(tabulate(trainy[dists[1:i]]+1))])
+  }
+  return(predictions)
 }
+
+knntests = list() 
+knntests[[i]] = knnclass(validation[,2:226],training[,2:226], training[,1], k=1:15)
 print("Done")
-save.image("~/knn.RData")
+save.image("knn.RData")
