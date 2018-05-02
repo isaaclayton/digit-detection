6d193120f932fcc310da3e33f705c3b953fae49b
Isaac Layton <isaacdlayton@gmail.com>
2018-05-01 18:34:35 -0700

    adjusted test script

--- testScript.R
+++ testScript.R
@@ -2,7 +2,7 @@ load(".RData")
 if(!require(flexclust)) install.packages('flexclust', repos = "http://cran.us.r-project.org")
 library(flexclust)
 
-knntests[[i]] = knnclass(test[,2:226],training[,2:226], training[,1], k=6)
+test_data = knnclass(test[,2:226],training[,2:226], training[,1], k=6)
 print("Done")
 
 save.image("test.RData")
